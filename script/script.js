2aaf5aa3107a31a94a0fc84e5b60b344d520700b
hutyra.beniamin <hutyra.beniamin@outlook.com>
2020-06-13 14:35:38 +0200

    CodeReview 5

---
+++ script/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    var myMovies = JSON.parse(movies);
+    var content = "";
+    for (let i = 0; i < myMovies.length; i++) {
+        let movie = myMovies[i];
+        like = Math.floor(Math.random() * 100);
+        content += `
+<div class="movie_wrapper">
+    <div class="img_con">
+        <img src="${movie.image}">
+    </div>
+        <div class="mov_title">
+            <h3>${movie.title}</h3>
+            <div class="movie_inf"> Genre ${movie.genre}</div><br>
+            <div class="movie_inf"> Directed by ${movie.Directed}</div><br>
+            <div class="movie_inf"> Written by ${movie.Written}</div><br>
+            <div class="movie_inf"> Studio ${movie.Studio}</div><br>
+            <div class="counterDiv">
+                <p>Like</p>
+            <span class="countBtn">
+            <img class="like" src="img/thumb.png" alt="thumbs up">
+        </span>
+        <div class="countNum">
+            <h1 class="headerCounter">${like}</h1>
+        </div>
+        </div>
+        
+    </div>
+</div>
+`;
+    }
+    $("#container").html(content);
+
+    var movieLikeCounter = $('.like');
+    var countNum = $('.headerCounter');
+    for (let i = 0; i < movieLikeCounter.length; i++) {
+        $(movieLikeCounter[i]).on('click', countUp);
+
+        function countUp() {
+            countNum[i].innerHTML++;
+        }
+    }
+
+});

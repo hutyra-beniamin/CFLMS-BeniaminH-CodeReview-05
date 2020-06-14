8d6944f0c8ff25cfab26c7fbd648b22422184ac7
hutyra.beniamin <hutyra.beniamin@outlook.com>
2020-06-14 16:27:46 +0200

    add more styling

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+//function to paste the content from json file to html
 $(document).ready(function() {
     var myMovies = JSON.parse(movies);
     var content = "";
@@ -19,17 +20,17 @@ $(document).ready(function() {
                 <p>Like</p>
             <span class="countBtn">
             <img class="like" src="img/thumb.png" alt="thumbs up">
-        </span>
-        <div class="countNum">
-            <h1 class="headerCounter">${like}</h1>
-        </div>
+            </span>
+            <div class="countNum">
+                <h1 class="headerCounter">${like}</h1>
+            </div>
         </div>
         
     </div>
 </div>
 `;
     }
-    $("#container").html(content);
+    $("#container").html(content); //The Like button function
 
     var movieLikeCounter = $('.like');
     var countNum = $('.headerCounter');
@@ -40,5 +41,26 @@ $(document).ready(function() {
             countNum[i].innerHTML++;
         }
     }
+    //function for sorting 
+    $("#pop").on("click", function() {
+        let num_order = $(".movie_wrapper").sort(function(x, y) {
+            return $(y).find("h1").text() - $(x).find("h1").text()
+        })
+        $("#container").html(num_order)
+    })
+
+    $("#lea").on("click", function() {
+        let num_order_inverse = $(".movie_wrapper").sort(function(x, y) {
+            return $(x).find("h1").text() - $(y).find("h1").text()
+        })
+        $("#container").html(num_order_inverse)
+    })
+
+    $("#abc").on("click", function() {
+        let abc = $(".movie_wrapper").sort(function(x, y) {
+            return $(x).find("h3").text() < $(y).find("h3").text() ? -1 : 1
+        })
+        $("#container").html(abc)
+    })
 
 });
